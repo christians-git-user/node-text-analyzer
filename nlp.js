979935bfbf7142f701f86286b78127be2f1e7e81
healtrex <christian.steckler@gmail.com>
2019-12-31 08:20:29 -0500

    stemmer and pos

--- nlp.js
+++ nlp.js
@@ -1,26 +1,59 @@
-const {pipe, map} = require(`sanctuary`);
+const S = require ('sanctuary');
 const stopwords = require(`./stopwords.js`).english
-const {getBrownCorpus} = require(`./getTestData.js`)
+const {stemmer} = require(`./stemmer.js`)
+var WordPOS = require('wordpos'),
+    wordpos = new WordPOS();
 
 // str -> str
 const toLowerCase = x => x.toLowerCase();
 
 // :: str -> str
-const replace = x => y => x.replace(y)
+const replace = x => y => z => x.replace(y, z)
 
 // :: str -> str
-const remove = x => replace(x)(``);
+const remove = x => y => replace(x)(y)(``);
+
+const removeLineBreaks = x => remove(x)(/(\r\n|\n|\r)/gm)
+
+const removePunctuation = x => remove(x)(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g)
+
+// fix this
+const isStopword = x => stopwords.includes(x);
+
+const removeStopwords = x => S.reject(isStopword)(x);
 
 // :: str -> array
 const split = x => y => x.split(y);
 
 const tokenize = x => split(x)(` `);
 
-const nlp = pipe([
+const stemAllTokens = x => S.map(stemmer)(x)
+
+const getPos = x => wordpos.getPOS(x);
+
+const getCounts = x => x.reduce(function (acc, curr) {
+    if (typeof acc[curr] == 'undefined') {
+      acc[curr] = 1;
+    } else {
+      acc[curr] += 1;
+    }
+  
+    return acc;
+  }, {});
+
+  const sort = x => x.sort()
+
+const nlp = S.pipe([
+    removeLineBreaks,
+    removePunctuation,
     toLowerCase,
     tokenize,
+    removeStopwords,
+    stemAllTokens,
+    getCounts,
+    // sort,
     console.log
-])
+]);
 
 const test = `THE DOORS 
 of the ~D train slid shut, and as I dropped into a seat and, 
@@ -30,7 +63,8 @@ the wide cheekbones, olive-flushed, that betrayed the Cherokee strain
 in her Midwestern lineage, and the mouth whose only fault, in the 
 novelist's carping phrase, was that the lower lip was a trifle too 
 voluptuous. From what I was able to gauge in a swift, greedy glance, 
-the figure inside the coral-colored boucle dress was stupefying.
+the figure inside the coral-colored boucle dress was stupefying. drop drop
 `
 
 nlp(test)
+// getPos(test).then(console.log)
