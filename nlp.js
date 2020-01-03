d5b806478ea8c8f7abee0140a258ede795b1403e
healtrex <christian.steckler@gmail.com>
2020-01-03 18:15:23 -0500

    refactoring nlp pipeline

--- nlp.js
+++ nlp.js
@@ -1,65 +1,83 @@
 const S = require ('sanctuary');
-const stopwords = require(`./stopwords.js`).english
-const {stemmer} = require(`./stemmer.js`)
+const stopwords = require('./stopwords.js').english
+const {stemmer} = require('./stemmer.js')
 // var WordPOS = require('wordpos'),
 //     wordpos = new WordPOS();
 
-// str -> str
-const toLowerCase = x => x.toLowerCase();
-// :: str -> str
-const replace = x => y => z => x.replace(y, z)
-// :: str -> str
-const remove = x => y => replace(x)(y)(``);
-// :: str -> str
-const removeLineBreaks = x => remove(x)(/(\r\n|\n|\r)/gm)
-// :: str -> str
-const removePunctuation = x => remove(x)(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g)
-// :: str -> boolean
-const isStopword = x => stopwords.includes(x);
-// :: [] -> []
-const removeStopwords = x => S.reject(isStopword)(x);
-// :: str -> array
-// const split = x => y => x.split(y);
-// :: str -> array
-const tokenize = x => x.split(` `);
-// :: [] -> []
-const stemAllTokens = x => S.map(stemmer)(x)
-// :: str -> object
-// const getPos = x => wordpos.getPOS(x);
+const nlp = x => {
+  const stringify = x => x.toString()
 
-const stringify = x => {
-  console.log(x)
-  return x.toString()
-}
+  // :: str -> str
+  const toLowerCase = x => x.toLowerCase();
 
-const getCounts = x => x.reduce(function (acc, curr) {
-    if (typeof acc[curr] == 'undefined') {
-      acc[curr] = 1;
-    } else {
-      acc[curr] += 1;
-    }
-    return acc;
-  }, {}
-);
+  // :: str -> str
+  const replace = x => y => z => x.replace(y, z)
 
-// :: str -> object
-const nlp = S.pipe([
+  // :: str -> str
+  const remove = x => y => replace(x)(y)('');
+
+  // :: str -> str
+  const removeLineBreaks = x => remove(x)(/(\r\n|\n|\r)/gm)
+
+  // :: str -> str
+  const removePunctuation = x => remove(x)(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g)
+
+  // :: str -> boolean
+  const isStopword = x => stopwords.includes(x);
+
+  // :: [] -> []
+  const removeEnglishStopwords = x => S.reject(isStopword)(x);
+
+  // :: str -> array
+  const split = x => y => x.split(y);
+
+  // :: str -> array
+  const tokenize = x => x.split(` `);
+
+  // :: [] -> []
+  const stemAllTokens = x => S.map(stemmer)(x)
+
+  // :: str -> object
+  // const getPos = x => wordpos.getPOS(x);
+
+  const getCounts = x => x.reduce(function (acc, curr) {
+      if (typeof acc[curr] == 'undefined') {
+        acc[curr] = 1;
+      } else {
+        acc[curr] += 1;
+      }
+      return acc;
+    }, {}
+  );
+
+  // :: str -> str
+  const cleanText = S.pipe([
+    //     -> str
     stringify,
     // str -> str
     removeLineBreaks,
     // str -> str
     removePunctuation,
     // str -> str
-    toLowerCase,
-    // str -> array
-    tokenize,
-    // array -> arraysplit(x)
-    removeStopwords,
-    // array -> array
-    stemAllTokens,
-    // array -> object
-    getCounts
-]);
+    toLowerCase
+  ])
+
+  // :: str -> object
+  const nlpPipeline = S.pipe([
+      // str -> str
+      cleanText,
+      // str -> array
+      tokenize,
+      // array -> array
+      removeEnglishStopwords,
+      // array -> array
+      stemAllTokens,
+      // array -> object
+      getCounts
+  ]);
+
+  return nlpPipeline(x)
+}
 
 module.exports = {
   nlp
