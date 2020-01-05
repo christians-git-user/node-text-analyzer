796ff39ecb2bc824e75008054f7b44532a76f178
healtrex <christian.steckler@gmail.com>
2020-01-05 11:24:55 -0500

    reorg

--- test/nlp.spec.js
+++ test/nlp.spec.js
@@ -1,8 +1,23 @@
 const {expect} = require('chai');
 
-describe('nlp', () => {
-    it('passes', () => {
-        expect(true)
-    })
+const {nlp} = require('../nlp.js')
 
+describe('nlp', () => {
+    it('return an object', () => {
+        const testBody = {
+            input: 'test test test test'
+        }
+        const test = nlp(testBody)
+        console.log(test)
+        expect(test).to.be.a('object')
+    })
+    it('when given an input with a string, nlp will count the number of stemmed words in that string', () => {
+        const testBody = {
+            input: 'test test test test'
+        }
+        const result = { test: 4 };
+        const test = nlp(testBody)
+        console.log(test)
+        expect(test).to.deep.equals(result)
+    })
 });
