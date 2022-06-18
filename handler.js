f921b6fab0f3a3a9735a9ee0fd0473c8dd6d1dda
Christian Steckler <christian.steckler@gmail.com>
2022-06-18 08:37:55 -0400

    changes

--- handler.js
+++ handler.js
@@ -1,19 +1,34 @@
 const {nlp} = require('./nlp');
 
 module.exports.nlp = async event => {
-  const requestBody = JSON.parse(event.body)
+  try{
+    console.log('request event = ', event);
+    const requestBody = JSON.parse(event.body)
+    const analyzedText = nlp(requestBody)
 
-  const analyzedText = nlp(requestBody)
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        request: requestBody,
-        stemmed_word_count: analyzedText
-      },
-      null,
-      2
-    ),
-  };
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          request: requestBody,
+          stemmed_word_count: analyzedText
+        },
+        null,
+        2
+      ),
+    };
+  } catch(error){
+    
+    console.log('Error: ', error);
+    return {
+      statusCode: 400,
+      body: JSON.stringify(
+        {
+          Message: error
+        },
+        null,
+        2
+      ),
+    }
+  }
 };
